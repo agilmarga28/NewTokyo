5a4229f62724b9cd3f73984fece14756adfaeea9
agilmarga28 <142233857+agilmarga28@users.noreply.github.com>
2023-10-10 12:36:06 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,25 +2,25 @@ require("dotenv").config();
 
 module.exports = {
   token: process.env.TOKEN || 'TOKEN', // your discord bot token
-  prefix: process.env.PREFIX || 'PREFIX', // bot prefix
+  prefix: process.env.PREFIX || '$', // bot prefix
   owner: process.env.OWNERID || ['665165194850336778'], //your discord id
-  SpotifyID: process.env.SPOTIFYID || 'cb41529dc3bd4d8f8a240dbee0fff4e8', // spotify client id
-  SpotifySecret: process.env.SPOTIFYSECRET || 'bcca82f42930498aa385a8289fdf276b', // spotify client secret
+  SpotifyID: process.env.SPOTIFYID || '6c31645ffb004ab8b44d06f7b96d1b66', // spotify client id
+  SpotifySecret: process.env.SPOTIFYSECRET || '3618fdc0b4824cfd91a8d425dac32987', // spotify client secret
   mongourl: process.env.MONGO_URI || 'mongodb+srv://rlx:rlx@rlx1.qqf2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // MongoDb URL
   embedColor: process.env.COlOR || '#2B2928', // embed colour
-  logs: process.env.LOGS || 'CHANNEL_ID', // Discord channel id 
+  logs: process.env.LOGS || '958902667994878003', // Discord channel id 
   links: {
-    support: process.env.SUPPORT || 'https://dsc.gg/tec-yt',
-    invite: process.env.INVITE || 'https://dsc.gg/tec-yt',
-    vote: process.env.VOTE || 'https://dsc.gg/tec-yt',
-    bg: process.env.BG || 'https://media.discordapp.net/attachments/1028936089555193887/1029023341312487505/musicplayer-home.png'
+    support: process.env.SUPPORT || 'https://discord.gg/s7SeG7jQXQ',
+    invite: process.env.INVITE || 'https://discord.gg/s7SeG7jQXQ',
+    vote: process.env.VOTE || 'none',
+    bg: process.env.BG || 'http://media.discordapp.net/attachments/571068093493805067/1158261860995436624/images_1_20.jpg?ex=651b9aeb&is=651a496b&hm=e0d45a9854097986a51c40d24b139edbc00d4cf27496aab6ea036785d17d6ea6&'
   },
 
   nodes: [
     {
-      url: process.env.NODE_URL || 'lava.horizxon.xyz:80',
+      url: process.env.NODE_URL || 'narco.buses.rocks:2269',
       name: process.env.NODE_NAME || 'Main',
-      auth: process.env.NODE_AUTH || 'horizxon.xyz',
+      auth: process.env.NODE_AUTH || 'glasshost1984',
       secure: parseBoolean(process.env.NODE_SECURE || 'false'),
     },
   ],
